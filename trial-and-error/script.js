95d5d3584ecfd08973c53057c71b0e1e840afb50
Shruthi Kamath <shruthi.dkamath6@gmail.com>
2023-06-20 13:12:22 +0530

    Advanced examples

---
+++ trial-and-error/script.js
@@ -0,0 +1,38 @@
+// function addAndMultiply(a, b){
+//     return[a+b, a*b]
+// }
+// const[sum, product] = addAndMultiply(2,3)
+// console.log(sum)
+// console.log(product)
+//-----------------------------
+// const array = ["a", "b", "c", "c"]
+// const person = {
+//     name: "Shruthi",
+//     age: 27,
+//     favoritefood: "rice",
+//     address: 
+//     {
+//         street: "123 main st",
+//         city: "Bangalore"
+//     }
+// }
+//---------------------------------------
+// const { address: {street}} = person
+// console.log(street)
+
+// function nameToFirstAndLast (fullName) {
+//    // array: return fullName.split(" ") 
+//    // object:
+//    const [firstName, lastName] = fullName.split(" ")
+//    return {
+//     firstName: firstName,
+//     lastName: lastName
+//    }
+
+// }
+
+// //const [firstName, lastName] = nameToFirstAndLast("Shruthi Kamath")
+// const {firstName, lastName} = nameToFirstAndLast("Shruthi Kamath")
+// console.log(firstName)
+// console.log(lastName)
+//----------------------------------
